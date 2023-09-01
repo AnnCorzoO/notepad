e3a9b8d74defe7664faa16b5b32fb23f24003ca7
AnnC <theangelcorzo@gmail.com>
2023-08-31 18:59:46 -0600

    app upload full

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,36 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    
   },
-  plugins: [],
+
+  
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#f9631d",
+        
+"secondary": "#efffa8",
+        
+"accent": "#c587ff",
+        
+"neutral": "#2e3338",
+        
+"base-100": "#e3e9f2",
+        
+"info": "#3358eb",
+        
+"success": "#55d89d",
+        
+"warning": "#f49e0b",
+        
+"error": "#fb3737",
+        },
+      },
+    ],
+  },
+
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 } satisfies Config;
