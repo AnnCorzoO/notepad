9b8c0682bb7d8885e7242d6a8a2e98f629de0162
AnnC <theangelcorzo@gmail.com>
2023-09-04 20:41:18 -0600

    added alerts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,6 +28,7 @@ export default Home;
 
 type Topic = RouterOutputs["topic"]["getAll"][0];
 
+
 const Content: React.FC = () => {
   const { data: sessionData } = useSession();
 
@@ -75,12 +76,13 @@ const Content: React.FC = () => {
       void refetchNotes();
     },
   });
+  const [isExpanded, setIsExpanded] = useState<boolean>(true);
 
   return (
     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
       <div className="px-2">
         <div className="text-size-2 font-extrabold text-primary"> Folders </div>
-        <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          <ul className="menu rounded-box w-56 bg-impact p-2 shadow-xl">
           {topics?.map((topic) => (
             <li key={topic.id}>
               <a
@@ -93,7 +95,7 @@ const Content: React.FC = () => {
                 {topic.title}
               </a>
               <button
-                className="w-18 btn btn-warning btn-sm justify-items-end text-center"
+                className="btn btn-warning btn-sm w-16 justify-center text-center text-gray-50 font-extralight"
                 onClick={() => void deleteTopic.mutate({ id: topic.id })}
               >
                 Delete
@@ -115,6 +117,24 @@ const Content: React.FC = () => {
             }
           }}
         />
+<div className="py-2">
+        <div className="alert">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+  <span>Create a "Folder" first in order to create a note.</span>
+</div>
+</div>
+<div className="py-2">
+  <div className="alert alert-warning">
+  <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+  <span>A Folder need to be created first in order to save a Note.</span>
+</div>
+</div>
+<div className="py-2">
+  <div className="alert alert-error">
+  <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+  <span>To delete a Folder, delete the Notes first.</span>
+</div>
+</div>
       </div>
       <div className="col-span-3">
         <div className="text-2xl text-primary"> Notes </div>
