cdf42ff761cce53b51799c494662d975166bd50d
AnnC <theangelcorzo@gmail.com>
2023-09-06 11:13:13 -0600

    folders and notes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,22 @@ const Home: NextPage = () => {
       <main>
         <Header />
         <Content />
+          <footer className="footer footer-center p-10 bg-primary text-primary-content">
+  <aside>
+    
+    <p className="font-bold">
+      Angel Corzo - AnnTheKat <br/>Jr React&Next Dev
+    </p> 
+    <p>Copyright © 2023 - All right reserved</p>
+  </aside> 
+  <nav>
+    <div className="grid grid-flow-col gap-4">
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a> 
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
+      <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+    </div>
+  </nav>
+</footer>
       </main>
     </>
   );
@@ -84,9 +100,9 @@ const Content: React.FC = () => {
           <input type="checkbox" className="peer" />
         <div className="text-size-2 font-extrabold text-black collapse-title bg-primary"> Folders </div>
           <div className="collapse-content flex items-center bg-accent py-5">
-          <ul className="menu rounded-box max-w:sm bg-impact p-2 shadow-xl flex-row bg-base-100">
+          <ul className="menu rounded-box max-w:sm bg-impact p-2 shadow-xl flex-col bg-base-100">
           {topics?.map((topic) => (
-            <li key={topic.id}>
+            <li key={topic.id} className="flex justify-start">
               <a
                 href="#"
                 onClick={(evt) => {
@@ -96,11 +112,10 @@ const Content: React.FC = () => {
               >
                 {topic.title}
               </a>
-              <button
-                className="btn btn-warning btn-sm w-16 justify-center text-center text-gray-50 font-light flex"
+           <button
+                className="btn glass btn-outline btn-error btn-xs w-16 text-gray-50 font-light justify-center items-center text-center"
                 onClick={() => void deleteTopic.mutate({ id: topic.id })}
-              >
-                Delete
+              >Delete
               </button>
             </li>
           ))}
@@ -122,14 +137,14 @@ const Content: React.FC = () => {
           }}
         />
 <div className="py-2">
-        <div className="alert">
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <div className="alert alert-">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-white shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
   <span>Create a Folder first in order to create a note.</span>
 </div>
 </div>
 <div className="py-2">
   <div className="alert alert-warning">
-  <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6 bg-" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
   <span>A Folder need to be created first in order to save a Note.</span>
 </div>
 </div>
@@ -140,9 +155,10 @@ const Content: React.FC = () => {
 </div>
 </div>
       </div>
-      <div className="col-span-3">
-        <div className="text-2xl text-black"> Notes </div>
-        <div>
+      <div className="col-span-3 collapse">
+          <input type="checkbox" className="peer"></input>
+        <div className="text-2xl text-black collapse-title"> Notes </div>
+        <div className="collapse-content bg-primary">
           {notes?.map((note) => (
             <div key={note.id} className="mt-5">
               <NoteCard
@@ -162,7 +178,10 @@ const Content: React.FC = () => {
             });
           }}
         />
+
+        
       </div>
-    </div>
+    </div>    
+    
   );
 };
